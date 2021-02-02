a3208d3f534b3de70bd1ddf599f521c7bef82146
Md. Mazharul Islam <mazharulislam611@gmail.com>
2021-02-02 07:52:43 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,24 @@
+//falsy---------------
+//0
+//"";
+//undefine
+//false
+//null
+//NaN
+
+
+//Truthy...........
+//"0"
+//" "
+//true
+//[]
+//{}
+
+let num = 4;
+
+if (num){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
