bf05c1a25c60b1ce656fc341d40e076d1eb98add
Md. Mazharul Islam <mazharulislam611@gmail.com>
2021-02-02 17:30:22 +0600

    scopr

---
+++ array-split.js
@@ -0,0 +1,3 @@
+const num = [2, 4, 6, 8, 9, 12];
+const part1 = num.slice(2, 5);
+console.log(part1);
---
+++ scope.js
@@ -0,0 +1,20 @@
+let bonus = 32;
+
+function sum(num1, num2){
+    let result = num1 + num2 + bonus;
+    console.log(bonus);
+    
+    if(result > 9){
+        var mood = "happy";
+        mood = "goppy";
+        mood = 'loly';
+        mood = "Boby";
+        console.log(mood);
+    }
+    console.log(mood);
+    return result;
+    
+}
+
+const result = sum(7, 5);
+// console.log(result);
