8dffcba1f7af0cbc3d106bdac32cb0adf7618aa1
Md. Mazharul Islam <mazharulislam611@gmail.com>
2021-02-02 08:23:14 +0600

    null-undefine

---
+++ null-vs-undefined.js
@@ -0,0 +1,21 @@
+let name;
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+const parson = {name: 'Hashem', phone: 01465456, address: 'Dhaka'};
+console.log(parson.id);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [12, 13, 15];
+console.log(ages[14]);
+
